db3726adfd97ad7e2830a368b702477a05d50eae
Jebakani <vjishwarya@gmail.com>
2021-08-04 20:08:21 +0530

    [JEBAKANI] Checking the above uc for new tables

---
+++ InsertIntoProcedute.sql
@@ -0,0 +1,15 @@
+﻿create procedure InsertIntoAddressBook
+(
+  @firstName varchar(100),
+  @lastName varchar(100),
+  @address varchar(200),
+  @city varchar(50),
+  @state varchar(100),
+  @zipCode bigint,
+  @phoneNumber bigint,
+  @email varchar(50)
+)
+as
+begin
+insert into AddressBookTable(firstName,lastName,address,city,state,zipCode,phoneNumber,email) values(@firstName,@lastName,@address,@city,@state,@zipCode,@phoneNumber,@email)
+end
