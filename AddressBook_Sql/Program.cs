5c106fecf66f526b71ecaded24023a1382abaedd
Jebakani <vjishwarya@gmail.com>
2021-08-04 15:40:34 +0530

    [JEBAKANI] Creating Address Book service database usingSql client

---
+++ AddressBook_Sql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_Sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book with database");
+        }
+    }
+}
